c9121b72e9bddd9ffd3d141655fc0859d28fd1f4
Ondra <ondrej.habr97@gmail.com>
2021-01-14 17:07:30 +0100

    app

---
+++ src/main/java/cz/habrondrej/projekt/ProjektApplication.java
@@ -0,0 +1,12 @@
+package cz.habrondrej.projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektApplication.class, args);
+    }
+}
